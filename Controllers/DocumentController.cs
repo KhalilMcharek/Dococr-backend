34f55b4dabf3bd935ced25f475e3240004f46fd9
KhalilMcharek <mcharek1khalil@gmail.com>
2025-07-16 10:18:36 +0100

    2 eme commit

---
+++ Controllers/DocumentController.cs
@@ -0,0 +1,66 @@
+﻿using Documents_OCR_back.Models.DTOs;
+using Documents_OCR_back.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Documents_OCR_back.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize] 
+    public class DocumentController : ControllerBase
+    {
+        private readonly DocumentService _documentService;
+
+        public DocumentController(DocumentService documentService)
+        {
+            _documentService = documentService;
+        }
+        [HttpPost("upload")]
+        [Consumes("multipart/form-data")]
+        public async Task<IActionResult> UploadDocument([FromForm] IFormFile file, [FromForm] string? documentName)
+        {
+            if (file == null || file.Length == 0)
+                return BadRequest("Aucun fichier envoyé.");
+
+            var userId = int.Parse(User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value);
+
+            var fileName = !string.IsNullOrEmpty(documentName) ? documentName : Path.GetFileName(file.FileName);
+
+            var textExtracted = "Texte extrait";
+
+            var doc = await _documentService.UploadDocumentAsync(fileName, textExtracted, userId);
+
+            return Ok(new { message = "Document uploadé", document = doc });
+        }
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetDocuments()
+        {
+            var userId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+            var docs = await _documentService.ListeDocumentAsync(userId);
+            return Ok(docs);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetDocument(int id)
+        {
+            var doc = await _documentService.DocumentById(id);
+            if (doc == null)
+                return NotFound(new { message = "Document introuvable" });
+
+            return Ok(doc);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDocument(int id)
+        {
+            await _documentService.DeleteDocumentAsync(id);
+            return Ok(new { message = "Document supprimé avec succès" });
+        }
+    }
+}
---
+++ Services/DocumentService.cs
@@ -0,0 +1,46 @@
+﻿using Documents_OCR_back.Data;
+using Documents_OCR_back.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+namespace Documents_OCR_back.Services
+
+{
+    public class DocumentService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DocumentService(ApplicationDbContext context)
+        {
+            _context = context;
+        }      
+         //upload 
+        public async Task<Document> UploadDocumentAsync(string filename, string textExtracted, int userId)
+        {
+            var Doc = new Document
+            { FileName = filename, TextExtracted = textExtracted, CorrectedText = "", UserId = userId, UploadedAt = DateTime.Now };
+            _context.Documents.Add(Doc);
+            await _context.SaveChangesAsync();
+            return Doc;
+        }
+        // historique ( liste)
+        public async Task<List<Document>> ListeDocumentAsync( int userId)
+        {
+            return await _context.Documents.Where(d => d.UserId == userId).ToListAsync();
+        }
+         //document par id 
+        public async Task <Document> DocumentById ( int id)
+        {
+            return await _context.Documents.FirstOrDefaultAsync(d => d.Id == id);
+        }
+        
+        //supprimer 
+        public async Task DeleteDocumentAsync(int id)
+        {
+            var doc = await _context.Documents.FindAsync(id);
+            if (doc == null)
+                throw new Exception("Document not found");
+
+            _context.Documents.Remove(doc);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
